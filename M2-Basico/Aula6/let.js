1aef2cc54abfffd6f6ae8d05d79bbb1ca744958a
jhennemann <jhennemannjha@gmail.com>
2025-03-22 15:40:57 -0300

    22/03 15:40

--- M2-Basico/Aula6/let.js
+++ M2-Basico/Aula6/let.js
@@ -1,7 +1,7 @@
 let nome; // Declarou a variável
 nome = 'João'; // Inicializando a variável
 console.log(nome);
-nome = 'Otávio';
+nome = 'Otávioo';
 console.log(nome, 'nasceu em 1984.'); 
 console.log('Em 2000', nome, 'conheceu Maria. ');
 console.log(nome, 'casou-se com Maria em 2012.');
